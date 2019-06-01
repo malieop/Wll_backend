828d7bc07767d9814c5d6a3bd445271cbadf2a1a
João Miranda <joao.miranda.ceres@radixeng.com.br>
2019-06-01 19:46:45 +0100

    Updated configuration to add support to tomcat deployment

--- src/main/java/pt/feup/worldlivelink/WorldLiveLinkApplication.java
+++ src/main/java/pt/feup/worldlivelink/WorldLiveLinkApplication.java
@@ -3,17 +3,24 @@ package pt.feup.worldlivelink;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import pt.feup.worldlivelink.Storage.StorageProperties;
 import pt.feup.worldlivelink.Storage.StorageService;
 
 @SpringBootApplication
 @EnableConfigurationProperties(StorageProperties.class)
-public class WorldLiveLinkApplication {
+public class WorldLiveLinkApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WorldLiveLinkApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(WorldLiveLinkApplication.class);
 	}
 
 	@Bean
